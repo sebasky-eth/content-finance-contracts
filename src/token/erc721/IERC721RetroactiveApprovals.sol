600e49bd60346e829694f2b1c761a04e1302dc77
sebasky-eth <sebasky.eth@gmail.com>
2025-07-04 17:32:40 +0200

    docs: improve IERC721RetroactiveApprovals comments

--- src/token/erc721/IERC721RetroactiveApprovals.sol
+++ src/token/erc721/IERC721RetroactiveApprovals.sol
@@ -11,35 +11,30 @@ interface IERC721RetroactiveApprovals is IERC721 {
     event RetroactiveApproval(address indexed owner, uint256 retroactiveDate);
 
     /**
-     * @notice Disable all approvals for caller without deleting them.
-     * @dev Doesn't emit classic IERC721 approval events.
-     * @param retroactiveDate - If 0 is passed, then all approvals will be working as usual. Cannot be bigger than current date.
-     * If time is bigger than 0, it will disable all approvals that was made before it.
+     * @notice Returns a date, which turns off all approvals of 'owner' granted before it.
      */
-    function disableAllApprovalsBefore(uint256 retroactiveDate) external;
+    function retroactiveDate(address owner) external view returns (uint256);
 
     /**
-     * @notice Disable all current approvals for caller without deleting them.
-     * Can be enabled again with: disableAllApprovalsBefore(0).
-     * Equivalent to disableAllApprovalsBefore(block.timestamp + 1); if argument like that would be allowed
-     */
-    function disableAllApprovals() external;
-
-    /**
-     * @notice Returns date when operator approval was changed by 'owner'.
-     * @return date - time when latest approval was made. '0' could mean that approval was never changed.
+     * @notice Returns time when IERC721.setApprovalForAll was made. '0' - is not approved.
      */
     function approvalDate(address owner, address operator) external view returns (uint256 date);
 
     /**
-     * @notice Returns date when token approval was lastly changed.
-     * @return date - time when latest approval was made. '0' mean that approval was never changed in current ownership.
+     * @notice Returns date when IERC721.getApproved was made. '0' - is not approved.
      */
     function tokenApprovalDate(uint256 tokenId) external view returns (uint256 date);
 
     /**
-     * @notice Returns a date, which turns off all approvals for 'owner' granted before it.
-     * @return date - time when latest approval was made. '0' mean that approval was never changed.
+     * @notice Disable all caller's approvals that was made before 'date'.
+     * @dev Emit RetroactiveApproval(date).
      */
-    function retroactiveDate(address owner) external view returns (uint256);
+    function disableAllApprovalsBefore(uint256 date) external;
+
+    /**
+     * @notice Disable all current caller's approvals.
+     * Can be enabled again with: disableAllApprovalsBefore(0).
+     * @dev Emit RetroactiveApproval(block.timestamp + 1)
+     */
+    function disableAllApprovals() external;
 }
